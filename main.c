a1a90dd2ecb45acdd5dfde07acf25e70ef5f0283
padawan <padawan@debianUPEM-2016>
2016-12-19 14:40:58 +0100

    Premier essai !

---
+++ main.c
@@ -0,0 +1,14 @@
+/* * * * * * * * * * * * * * */
+/*      BATAILLE  NAVALE     */
+/*       par David Nasr      */
+/*       et Daphné Rose      */
+/* * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf("Bonjour :)\n");
+
+    return 0;
+}
