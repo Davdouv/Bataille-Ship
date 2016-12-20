f31efdf0f8f5d7687048afc58aad85400f28f3ba
padawan <padawan@debianUPEM-2016>
2016-12-20 19:41:54 +0100

    Bug fixed checHit() attackFleet()

--- version minimale/functions.c
+++ version minimale/functions.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include "functions.h"
 
 #define NDIM 11     // Grid dimensions
@@ -26,7 +27,7 @@ char** initMap() {
     return map;
 }
 
-// Give a value to each slots
+/* Give a value to each slots */
 void createMap(char **map) {
     int i, j;
     for (i = 0; i < NDIM; i++) {
@@ -51,7 +52,7 @@ void displayMap(char **map) {
     int i, j;
     for (i = 0; i < NDIM; i++) {
         for (j = 0; j < NDIM; j++) {
-            if (j>9 && i == 0)
+            if (j > 9 && i == 0)
                 printf("1%c", (map[i][j] - 10));    // Change the display (but not the value) for numbers >= 10
             else {
                 printf("%c ", map[i][j]);
@@ -62,22 +63,32 @@ void displayMap(char **map) {
     printf("\n");
 }
 
-/* Creates the maps (2 for each player) */
+/* Creates the fleet */
 
 Fleet createFleet() {
     Fleet my_fleet;
+
+    strncpy(my_fleet.carrier.name, "Carrier", 11);
     my_fleet.carrier.length = 2;
     my_fleet.carrier.orientation = 0;
     my_fleet.carrier.life = 2;
+
+    strncpy(my_fleet.battleship.name, "Battleship", 11);
     my_fleet.battleship.length = 3;
     my_fleet.battleship.orientation = 0;
     my_fleet.battleship.life = 3;
+
+    strncpy(my_fleet.cruiser.name, "Cruiser", 11);
     my_fleet.cruiser.length = 3;
     my_fleet.cruiser.orientation = 0;
     my_fleet.cruiser.life = 3;
+
+    strncpy(my_fleet.submarine.name, "Submarine", 11);
     my_fleet.submarine.length = 4;
     my_fleet.submarine.orientation = 0;
     my_fleet.submarine.life = 4;
+
+    strncpy(my_fleet.destroyer.name, "Destroyer", 11);
     my_fleet.destroyer.length = 5;
     my_fleet.destroyer.orientation = 0;
     my_fleet.destroyer.life = 5;
@@ -86,42 +97,46 @@ Fleet createFleet() {
 }
 
 /* START */
-/* Select a slot | put the line & the column values in variables*/
+/* Select a slot | put the line & the column values in variables l and c */
 void selectSlot(char **map, int *l, int *c) {
     int i;
-    char line;
+    char letter = ' ';
+    char *line = &letter;
 
     do {
-        printf("Which line ?\n");
-        scanf("%c", &line);
-        line = toupper(line);
-    } while(line<'A' || line>('A'+NDIM-2));          // Repeat until player choose a correct line
+        printf("Which line?\n");
+        scanf(" %c", line);
+        *line = toupper(*line);
+    } while(*line<'A' || *line>('A'+NDIM-2));          // Repeat until player choose a correct line
 
     do {
-        printf("Which column ?\n");
+        printf("Which column?\n");
         scanf("%d", c);
+        if (*c<1 || *c>=NDIM) {
+            printf("Please choose a correct column number.\n");
+        }
     } while(*c<1 || *c>=NDIM);                       // Repeat until player choose a correct column
     
-    
-    
-    //printf("You have choosen %c %d\n", line, *c);
     for (i = 0; i < NDIM; i++) {
-        if (map[i][0] == line)                      // Find the line number which countains the letter
+        if (map[i][0] == *line)                      // Find the line number which countains the letter
          *l = i;           
     }
 }
 
 int setOrientation() {
     int o;
-   // do {
-        printf("Which orientation ? (0: horizontal  1: vertical)\n");
+    do {
+        printf("Which orientation? (0: horizontal  1: vertical)\n");
         scanf("%d",&o);
-   // } while();                          // Repeat until player choose 0 or 1
+        if (o != 0 && o != 1) {
+            printf("Please choose between 0 or 1.\n");
+        }
+    } while(o != 0 && o != 1);                          // Repeat until player choose 0 or 1
    
     return o;
 }
 
-int checkPosition(char **map, int *l, int *c, int o, int ship_length) {
+int checkPlacement(char **map, int *l, int *c, int o, int ship_length) {
     int i;
     if (o == 0) {                                   // If it's horizontal
         for (i = 0; i<ship_length; i++) {
@@ -139,22 +154,37 @@ int checkPosition(char **map, int *l, int *c, int o, int ship_length) {
 }
 
 /* Players place their ships */
-void placeShip(char **map, int *l, int *c, Fleet *p_fleet) {
-    int i, o;
+void placeShip(char **map, int *l, int *c, Ship *p_ship) {
+    int i, o, checkposition;
     do {
-        printf("Set carrier (%d)\n", p_fleet->carrier.length);
         selectSlot(map, l, c);
         o = setOrientation();
-    } while (checkPosition(map, l, c, o, p_fleet->carrier.length)==0);
+        checkposition = checkPlacement(map, l, c, o, p_ship->length);
+        if (checkposition == 0) {
+            printf("You must replace your ship, it is stepping over another or it's out of the map.\n");
+        }
+    } while (checkposition == 0);
     if (o == 0) {
-        for (i = 0; i< p_fleet->carrier.length; i++) {
+        for (i = 0; i< p_ship->length; i++) {
             map[*l][*c+i]='*';
         }
     }
     else {
-        for (i = 0; i< p_fleet->carrier.length; i++) {
+        for (i = 0; i< p_ship->length; i++) {
             map[*l+i][*c]='*';
         }
+    }
+}
+
+void placeFleet(char **map, int *l, int *c, Fleet *p_fleet) {
+    int i;
+    Ship *current_ship; // pointer to the ship that is placed
+    current_ship = &(p_fleet->carrier); // pointer initialized to the first ship Carrier
+    for(i=0;i<5;i++) { 
+        printf("Set %s (%d)\n", current_ship->name, current_ship->length);
+        placeShip(map, l, c, current_ship);
+        displayMap(map);
+        current_ship += 1; // the pointer changes to the next ship 
     }
 }
 /* A player is randomly chosen to start */
@@ -162,4 +192,41 @@ void placeShip(char **map, int *l, int *c, Fleet *p_fleet) {
 /* TURN */
 /* Player gets the damages on his map (except for the first turn) */
 /* Player attacks */
+int checkHit(char **map_att, char **map_def, int *l, int *c) {
+    if (map_att[*l][*c] != '.') {
+        printf("You already shot here. Choose again\n");
+        return -1;                  // Already shot here
+    }
+    if (map_def[*l][*c] == '*') {
+        printf("Hit !\n");
+        return 1;                   // Ship
+    }
+        
+    else if (map_def[*l][*c] == '.') {
+        printf("Miss !\n");
+        return 0;                   // Water
+    }
+}
+
+void attackFleet(char **map_att, char **map_def, int *l, int *c, Fleet *p_fleet) {
+    int i, check = 1;
+    displayMap(map_att);
+    printf("It's your time to attack !\n");
+    do {
+        do {
+            selectSlot(map_att, l, c);
+            check = checkHit(map_att, map_def, l, c);
+        } while(check == -1);
+        
+        if (check == 1) {
+            map_att[*l][*c] = 'X';
+            printf("You can shoot again !\n");
+        }
+
+        if (check == 0) {
+            map_att[*l][*c] = 'O';
+        }
+        displayMap(map_att);
+    } while(check == 1);
+} 
 /* Player gets the result of his attack */
