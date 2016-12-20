92683751a5c6d727204d709e77a9250d75db239e
padawan <padawan@debianUPEM-2016>
2016-12-20 20:13:17 +0100

    Push main

--- version minimale/main.c
+++ version minimale/main.c
@@ -13,21 +13,25 @@
 
 int main() {
     int line, column;
-    char **p1_att, **p1_def;
+    char **p1_att, **p1_def, **p2_att, **p2_def;
 
-    Fleet p1_fleet;
+    Fleet p1_fleet, p2_fleet;
    
-    p1_att = initMap();
-    p1_def = initMap();
+    p1_att = p1_def = p2_att = p2_def = initMap();
+    //p1_fleet.carrier = {1,0,1};
     p1_fleet = createFleet();
    
     createMap(p1_att);
     createMap(p1_def);
+    createMap(p2_att);
+    createMap(p2_def);
    
     /* Map display (this is temporary, I just want to see what I'm doing) */
     displayMap(p1_def);
     placeFleet(p1_def, &line, &column, &p1_fleet);
-    attackFleet(p1_att, p1_def, &line, &column, &p1_fleet);
+   
+    // placeShip(p1_def, &line, &column, &p1_fleet);
+    // displayMap(p1_def);
 
     return 0;
 }
